0d558ac1297caa49580aa145727e9ab58c5f51de
whitelotus19 <159101935+centmarde@users.noreply.github.com>
2024-04-30 14:49:37 +0800

    co-developer
    an app for nail business
    
    Co-Authored-By: christinebersano <160454952+tiney2426@users.noreply.github.com>

--- my-app/src/js/router/router.js
+++ my-app/src/js/router/router.js
@@ -1,8 +1,7 @@
-import { errorNotification, successNotification, supabase } from "../main";
-
-let isLoggedIn = localStorage.getItem("access_token") !== null;
-
 function setRouter() {
+  let isLoggedIn = localStorage.getItem("access_token") !== null;
+  const userRole = localStorage.getItem("role");
+
   switch (window.location.pathname) {
     case "/":
     case "/index.html":
@@ -21,14 +20,17 @@ function setRouter() {
     case "/adminbooking.html":
     case "/adminprofile.html":
     case "/customer.html":
-
       // Allow access to items.html only if the user has the "Owner" or "Admin" role
-      if (!localStorage.getItem("access_token")/*  || (userRole !== "user" && userRole !== "Admin") */) {
+      if (
+        !localStorage.getItem(
+          "access_token"
+        ) /*  || (userRole !== "user" && userRole !== "Admin") */
+      ) {
         window.location.pathname = "/index.html"; // redirect to home page if not logged in or not an owner/admin
       }
       break;
 
-   /*  case "/dashboard.html":
+    /*  case "/dashboard.html":
       // Add more cases if there are more pages
       if (!isLoggedIn) {
         window.location.pathname = "/index.html"; // default page when logged out
